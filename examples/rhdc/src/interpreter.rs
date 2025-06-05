cc46d5948677203d6a7627cf887ff1db8557709c
ovenror <85027062+ovenror@users.noreply.github.com>
2025-06-05 21:24:08 +0200

    More useful argument parsing error handling

--- examples/rhdc/src/interpreter.rs
+++ examples/rhdc/src/interpreter.rs
@@ -33,16 +33,16 @@ pub trait Parameter : Sized {
         return "<".to_string() + Self::usage() + ">"
     }
 
-    fn complete(arg: &str) -> Vec<String>
+    fn complete(args: &str, from: usize) -> Vec<String>
     {
-        Self::completer().complete(arg)
+        Self::completer().complete(&args[from..])
     }
 
-    fn position(arg: &str) -> (usize, usize) {
-        match Self::regex().captures(arg) {
+    fn position(args: &str, from: usize) -> (usize, usize) {
+        match Self::regex().captures(&args[from..]) {
             Some(c) => {
                 let arg = c.get(0).unwrap();
-                (arg.start(), arg.end())
+                (from + arg.start(), from + arg.end())
             },
             None => (0, 0)
         }
@@ -58,17 +58,23 @@ pub trait Argument<'a> : Sized + Parameter
 {
     fn parse<'b>(arg: &'a str) -> Result<Self, &'b str>;
 
-    fn extract<'b>(args: &'a str) -> Result<(Self, usize), ExtractErr<'b>> {
-        Self::do_extract(args)
+    fn extract<'b>(args: &'a str, from: usize) -> Result<(Self, usize), ExtractErr<'b>> {
+        if from >= args.len() {
+            Err(ExtractErr::Match)
+        } else {
+            Self::do_extract(args, from)
+        }
     }
 
-    fn do_extract<'b>(args: &'a str) -> Result<(Self, usize), ExtractErr<'b>>
+    fn do_extract<'b>(args: &'a str, from: usize) -> Result<(Self, usize), ExtractErr<'b>>
     {
-        match Self::regex().captures(args) {
+        assert!(from < args.len());
+
+        match Self::regex().captures(&args[from..]) {
             Some(c) => {
                 let m = c.get(0).unwrap();
                 match Self::parse(m.as_str()) {
-                    Ok(arg) => Ok((arg, m.end())),
+                    Ok(arg) => Ok((arg, from + m.end())),
                     Err(e) => Err(ExtractErr::Parse(e))
                 }
             },
@@ -98,8 +104,12 @@ impl<P: Parameter> Parameter for Option<P> {
 }
 
 impl<'a, A: Argument<'a>> Argument<'a> for Option<A> {
-    fn extract<'b>(args: &'a str) -> Result<(Self, usize), ExtractErr<'b>> {
-        match Self::do_extract(args) {
+    fn extract<'b>(args: &'a str, from: usize) -> Result<(Self, usize), ExtractErr<'b>> {
+        if from >= args.len() {
+            return Ok((None, from))
+        }
+
+        match Self::do_extract(args, from) {
             Err(_) => Ok((None, 0)),
             ok => ok
         }
@@ -117,8 +127,15 @@ pub trait CommandCompleter {
     fn complete(&self, text: &str) -> Vec<String>;
 }
 
+pub enum ExecErr<'a> {
+    TooFew,
+    TooMany,
+    Match,
+    Parse(&'a str)
+}
+
 pub trait AbstractCommand<T> {
-    fn exec<'a>(&self, processor: &mut T, args: &str) -> Result<(), ExtractErr<'a>>;
+    fn exec<'a>(&self, processor: &mut T, args: &str) -> Result<(), (usize, ExecErr<'a>)>;
     fn complete(&self, text: &str) -> (usize, Vec<String>);
     fn name(&self) -> &'static str;
     fn param_usage(&self, err: &mut dyn Write) -> Result<(), std::io::Error>;
@@ -128,6 +145,25 @@ pub trait AbstractCommand<T> {
         self.param_usage(err).unwrap();
         writeln!(err).unwrap();
     }
+
+    fn indicator(&self, err: &mut dyn Write, pos: usize, extra: usize) {
+        writeln!(err, "{}^", String::from_utf8(vec![b' '; extra + self.name().len() + pos]).unwrap()).unwrap();
+    }
+
+    fn error(&self, err: &mut dyn Write, e: (usize, ExecErr), extra: usize) {
+        self.indicator(err, e.0, extra);
+        writeln!(err, "{}", match e.1 {
+            ExecErr::TooFew => "Too few arguments.",
+            ExecErr::TooMany => "Too many arguments.",
+            ExecErr::Match => "Parse error.",
+            ExecErr::Parse(reason) => reason,
+        }).unwrap();
+        self.usage(err);
+    }
+}
+
+fn lws(text: &str) -> usize {
+    text.len() - text.trim_start().len()
 }
 
 macro_rules! cmdimpl {
@@ -156,57 +192,67 @@ macro_rules! cmdimpl {
             }
 
             impl <T, $($param: Parameter),*> AbstractCommand<T> for [<Cmd $param_count>]<T, $($param),*> {
-                fn exec<'a>(&self, processor: &mut T, args: &str) -> Result<(), ExtractErr<'a>> {
-                    let mut last_end = 0;
+                fn exec<'a>(&self, processor: &mut T, args: &str) -> Result<(), (usize, ExecErr<'a>)> {
+                    let mut next_at = lws(args);
+                    let mut expect_next_at = 1;
 
                     $(
-                        let ([<arg_ $param:lower>], arg_end) = match $param::Arg::extract(&args[last_end..]) {
-                            Ok((arg, end)) => (arg, last_end + end),
-                            Err(e) => return Err(e)
+                        assert!(next_at <= args.len());
+
+                        let ([<arg_ $param:lower>], arg_end) = match $param::Arg::extract(&args, next_at) {
+                            Ok(result) => result,
+                            Err(e) => return Err(match e {
+                                ExtractErr::Match =>
+                                    if next_at == args.len() {
+                                        (expect_next_at, ExecErr::TooFew)
+                                    } else {
+                                        (next_at, ExecErr::Match)
+                                    },
+                                ExtractErr::Parse(reason) => (next_at, ExecErr::Parse(reason))
+                            })
                         };
 
                         let [<argref_ $param:lower>] = &[<arg_ $param:lower>];
-                        last_end = arg_end;
+                        next_at = arg_end + lws(&args[arg_end..]);
+                        expect_next_at = arg_end + 1;
                     )*
 
                     //More arguments than expected is an error
-                    if (last_end < args.len()) {
-                        return Err(ExtractErr::Match);
+                    if (next_at < args.len()) {
+                        return Err((next_at, ExecErr::TooMany));
                     }
 
                     //Prevent compiler from whining when there are no parameters
-                    let _ignore = (args, last_end);
-                    last_end = 0;
-                    let _ignore2 = last_end;
+                    let _ignore = (args, expect_next_at);
+                    next_at = 0;
+                    expect_next_at = 0;
+                    let _ignore2 = (next_at, expect_next_at);
 
                     Ok((self.func)(processor, $([<argref_ $param:lower>],)* ))
                 }
 
                 fn complete(&self, args: &str) -> (usize, Vec<String>) {
-                    let mut remaining_args = args;
                     let mut last_end = 0;
 
                     $(
-                        let (arg_start, arg_end) = match $param::position(remaining_args) {
-                            (0, 0) => return (last_end, $param::complete(remaining_args)),
-                            (start, end) => (last_end + start, last_end + end)
+                        let (arg_start, arg_end) = match $param::position(args, last_end) {
+                            (0, 0) => return (last_end, $param::complete(args, last_end)),
+                            pos => pos
                         };
 
                         assert!(arg_end <= args.len());
 
                         if arg_end == args.len() {
-                            return (arg_start, $param::complete(remaining_args));
+                            return (arg_start, $param::complete(args, last_end));
                         }
 
                         last_end = arg_end;
-                        remaining_args = &args[last_end..];
                     )*
 
                     //Prevent compiler from whining when there are no parameters
-                    let _ignore = (args, remaining_args, last_end);
-                    remaining_args = &args;
+                    let _ignore = (args, last_end);
                     last_end = 0;
-                    let _ignore2 = (remaining_args, last_end);
+                    let _ignore2 = last_end;
 
                     (0, Vec::new())
                 }
@@ -261,12 +307,7 @@ pub trait Processor : Sized {
 
         match command.exec(self, args) {
             Ok(_) => (),
-            Err(e) => match e {
-                ExtractErr::Match => command.usage(self.stderr()),
-                ExtractErr::Parse(reason) => {
-                    writeln!(self.stderr(), "{} failed: {}", cmd, reason).unwrap();
-                }
-            }
+            Err(e) => command.error(self.stderr(), e, 2)
         }
 
         true
